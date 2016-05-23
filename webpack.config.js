e4dabe56e9ef9b647b6dcb3c566c44d95e4b60cf
asolove <asolove@gmail.com>
2016-05-23 18:10:48 -0400

    Add Babel and es2015 preset. ~3s.

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,30 @@
 var webpack = require("webpack");
 
 module.exports = {
-    entry: {
-        page1: "./src/page1",
-        page2: "./src/page2"
-    },
-    output: {
-        path: './built',
-        filename: "[name].js",
-        chunkFilename: "[id].js"
-    },
-    plugins: [
-      new webpack.optimize.CommonsChunkPlugin({
-        name: 'common'
-      })
-    ]
+	entry: {
+		page1: "./src/page1",
+		page2: "./src/page2"
+	},
+	output: {
+		path: './built',
+		filename: "[name].js",
+		chunkFilename: "[id].js"
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: 'babel', // 'babel-loader' is also a legal name to reference
+				query: {
+					presets: ['es2015']
+				}
+			}
+		]
+	},
+	plugins: [
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'common'
+		})
+	]
 }
