dd1cb8fd22983f29b535309c078401a30eebb81b
asolove <asolove@gmail.com>
2016-05-23 19:32:18 -0400

    500ms startup time. Subsequent builds inside same process that much faster than the first.

--- build.js
+++ build.js
@@ -1,6 +1,14 @@
 var webpack = require("webpack");
 var config = require("./webpack.config.js");
 
-webpack(config).run(function(e, stats) {
-	console.log(stats.toString({ colors: true, chunks: false }));
-});
+function build(times) {
+  webpack(config).run(function(e, stats) {
+    console.log(stats.toString({ colors: true, chunks: false }));
+
+    if(times > 0) {
+      setTimeout(function() { build(times-1); }, 1);
+    }
+  });
+}
+
+build(20);
