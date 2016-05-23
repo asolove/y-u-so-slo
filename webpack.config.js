2b42785c70f05a0d259c2338bb7692c1e6f0b435
asolove <asolove@gmail.com>
2016-05-23 18:25:06 -0400

    Use cacheDirectory option. 4s->.5s on repeat builds.

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,8 @@ module.exports = {
 				exclude: /(node_modules|bower_components)/,
 				loader: 'babel', // 'babel-loader' is also a legal name to reference
 				query: {
-					presets: ['es2015', 'stage-0']
+					presets: ['es2015', 'stage-0'],
+          cacheDirectory: true
 				}
 			}
 		]
